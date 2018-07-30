105f0fdc5bd39232e5f17b671b3885e049d66432
THINHPHAN-PC\ThinhPhan <thinhpd6393@gmail.com>
2018-07-30 17:14:06 +0700

    - update UI

--- resources/views/admin/products/view.blade.php
+++ resources/views/admin/products/view.blade.php
@@ -137,7 +137,11 @@
         print_table_photos(photos);
     }
 
-    $(document).ready(function () {
+    function synchronize_child_and_parent_category($) {
+        
+    }
+
+    $(document).ready(function ($) {
         $( "#mainForm" ).submit(function( event ) {
             var searchIDs = $("#mainForm .list-tree-section input:checkbox:checked").map(function(){
               return $(this).val();
@@ -156,6 +160,58 @@
 
         print_table_details(details);
         print_table_photos(photos);
+        synchronize_child_and_parent_category($);
+
+
+            $('input[type="checkbox"]').change(function(e) {
+
+              var checked = $(this).prop("checked"),
+              container = $(this).parent(),
+              siblings = container.siblings();
+
+              container.find('input[type="checkbox"]').prop({
+                indeterminate: false,
+                checked: checked
+            });
+
+              function checkSiblings(el) {
+
+                var parent = el.parent().parent(),
+                all = true;
+
+                el.siblings().each(function() {
+                  return all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+              });
+
+                if (all && checked) {
+
+                  parent.children('input[type="checkbox"]').prop({
+                    indeterminate: false,
+                    checked: checked
+                });
+
+                  checkSiblings(parent);
+
+              } else if (all && !checked) {
+
+                  parent.children('input[type="checkbox"]').prop("checked", checked);
+                  parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+                  checkSiblings(parent);
+
+              } else {
+
+                  el.parents("li").children('input[type="checkbox"]').prop({
+            // indeterminate: true,
+            checked: false
+        });
+
+              }
+
+          }
+
+          checkSiblings(container);
+      });
+        
 
     });
 </script>
